a9ac4f2e771e056534d58ddd106c3cb8da272a6e
jaymanale <jaymanale29@gmail.com>
2023-04-28 08:37:21 +0530

    Mutating the data - Adding new tofo in the list

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,10 +1,47 @@
-import { useRef } from 'react';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRef } from "react";
+import { Todo } from "./hooks/useTodos";
+import axios from "axios";
 
 const TodoForm = () => {
+  const queryClient = useQueryClient();
+  const addTodo = useMutation({
+    mutationFn: (todo: Todo) =>
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", todo)
+        .then((response) => response.data),
+    onSuccess: (savedTodo, newTodo) => {
+      // APPROACH 1 : Invalidating cache  and ask to make new call - USE this approach always
+      // queryClient.invalidateQueries({
+      //   queryKey: ["todos"],
+      // });
+
+      // APPROACH 2 : updating data directly in cache as this API does not support actual Update call
+
+      queryClient.setQueriesData<Todo[]>(["todos"], (todos) => [
+        savedTodo,
+        ...(todos || []),
+      ]);
+    },
+  });
+
   const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <form className="row mb-3">
+    <form
+      className="row mb-3"
+      onSubmit={(event) => {
+        event.preventDefault();
+
+        if (ref.current && ref.current.value)
+          addTodo.mutate({
+            id: 0,
+            title: ref.current?.value,
+            completed: false,
+            userId: 1,
+          });
+      }}
+    >
       <div className="col">
         <input ref={ref} type="text" className="form-control" />
       </div>
